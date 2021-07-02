e78ed2d996c4555c48fd9aa5d07e2931b5093ec8
touyou <fujiyou1121@gmail.com>
2021-07-02 16:01:55 +0900

    Initial Commit

---
+++ GpAttendance/GpAttendanceApp.swift
@@ -0,0 +1,17 @@
+//
+//  GpAttendanceApp.swift
+//  GpAttendance
+//
+//  Created by emp-mac-yosuke-fujii on 2021/07/02.
+//
+
+import SwiftUI
+
+@main
+struct GpAttendanceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
